c0f74ae4a8e1f82d33f0f2d1446be8612ee67487
Jonathan Soileau <soileau_jonathan@yahoo.com>
2026-02-13 13:07:19 -0600

    React + Expo Setup Completed
    This is everything (I think) for the setup of React and Expo. If anything is wrong then let me know I guess.

--- Selu383.SP26.Api/Program.cs
+++ Selu383.SP26.Api/Program.cs
@@ -39,9 +39,29 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app
+    .UseRouting()
+    .UseAuthentication()
+    .UseAuthorization()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
 
-app.MapControllers();
+app.UseStaticFiles();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSpa(x =>
+    {
+        x.UseProxyToSpaDevelopmentServer("http://localhost:5173");
+
+    });
+}
+else
+{
+    app.MapFallbackToFile("/index.html");
+}
 
 app.Run();
 
